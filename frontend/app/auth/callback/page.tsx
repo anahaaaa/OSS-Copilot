b922559bce70b8522b19a17dbb091abbd913fb70
anahaaaa <anagha11165@gmail.com>
2026-06-07 14:29:22 +0530

    add const dynamic

--- frontend/app/auth/callback/page.tsx
+++ frontend/app/auth/callback/page.tsx
@@ -3,6 +3,8 @@
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default function CallbackPage() {
   const searchParams = useSearchParams();
 
