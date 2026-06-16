246127904350b27ab0e2a257b29b1365feb14e73
anahaaaa <anagha11165@gmail.com>
2026-06-16 22:15:10 +0530

    refactor: migrate database session management to FastAPI dependencies

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from typing import Generator
+from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase
 import os
 
 load_dotenv()
@@ -14,6 +15,14 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+def get_db() -> Generator:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
 
 class Base(DeclarativeBase):
     pass
